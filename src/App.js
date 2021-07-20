94aaef0770c673e098161fdcb21d13fdb5c189fe
ozkalai <melihozkalayy@gmail.com>
2021-07-20 11:22:31 +0300

    App component updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import List from "./components/List";
+
+function App() {
+  return (
+    <>
+      <div className="App">Country App</div>
+      <List />
+    </>
+  );
+}
+
+export default App;
