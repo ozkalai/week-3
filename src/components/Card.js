9db747be8660fa603198fda6d194e96c8ff31fac
ozkalai <melihozkalayy@gmail.com>
2021-07-21 17:10:31 +0300

    Card component updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaLanguage } from "react-icons/fa";
+import { GiModernCity } from "react-icons/gi";
 
 import "../styles/card.scss";
 
@@ -9,7 +11,30 @@ export default function Card({ item }) {
         <div className="card-flag">
           <img src={item.flag} alt="flag" />
         </div>
-        <div className="card-info">{item.name}</div>
+        <p className="card-name">{item.name}</p>
+        <div className="card-info">
+          {item.languages[0].name ? (
+            <div className="card-icon">
+              <FaLanguage />
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="card-info-lang">{`${item.languages[0].name}`}</div>
+          {item.capital ? (
+            <div className="card-icon">
+              <GiModernCity />
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="card-info-capital">
+            {item.capital
+              ? `
+          ${item.capital}`
+              : ""}
+          </div>
+        </div>
       </div>
     </div>
   );
