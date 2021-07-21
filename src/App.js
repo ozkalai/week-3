f9afeaf1c40c3e782e31790c70f77d127f50dbbf
ozkalai <melihozkalayy@gmail.com>
2021-07-21 17:09:50 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import React, { useState } from "react";
 import List from "./components/List";
+import ListButton from "./components/ListButton";
+import StatisticsButton from "./components/StatisticsButton";
+import Statistics from "./components/Statistics";
+import "../src/styles/buttons.scss";
 
 function App() {
+  const [showList, setShowList] = useState(true);
   return (
     <>
       <div className="App">Country App</div>
-      <List />
+      <div className="buttons">
+        <ListButton showList={showList} setShowList={setShowList} />
+        <StatisticsButton showList={showList} setShowList={setShowList} />
+      </div>
+      {showList ? <List /> : <Statistics />}
     </>
   );
 }
