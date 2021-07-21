4a15400f041b478e6bef3ecb0d65cc9b35922f87
ozkalai <melihozkalayy@gmail.com>
2021-07-21 17:11:01 +0300

    Statistics component created

---
+++ src/components/Statistics.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/statistics.scss";
+
+export default function Statistics() {
+  return (
+    <div className="container">
+      <div>this is statics page</div>
+    </div>
+  );
+}
